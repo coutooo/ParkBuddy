ac84bd5f1ea11bb705c4cffc6ec1329e4d0e2a6a
Afonso Teixeira <apteixeira@ua.pt>
2022-10-18 22:24:15 +0100

    profile

---
+++ parkbuddy/lib/Screens/Profile/profile_page.dart
@@ -0,0 +1,155 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:parkbuddy/Screens/Welcome/register_page.dart';
+import 'package:parkbuddy/Screens/main_page.dart';
+import 'package:parkbuddy/Screens/qr_page.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+        backgroundColor: Color.fromRGBO(160, 5, 10, 40),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+      ),
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  width: width * 0.8,
+                  height: height * 0.28,
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      double inHeight = constraints.maxHeight;
+                      double inWidth = constraints.maxWidth;
+                      return Stack(
+                        children: [
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              height: inHeight * 0.65,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.white,
+                              ),
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 80,
+                                  ),
+                                  Text(
+                                    'Afonso Couto',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 30,
+                                      fontFamily: 'Nunito',
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            child: Center(
+                              child: Container(
+                                width: inWidth * 0.49,
+                                child: Image.asset(
+                                  'assets/images/profile.png',
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  height: height * 0.5,
+                  width: width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'My Orders',
+                          style: TextStyle(
+                            color: Color.fromRGBO(39, 105, 171, 1),
+                            fontSize: 27,
+                            fontFamily: 'Nunito',
+                          ),
+                        ),
+                        Divider(
+                          thickness: 2.5,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: height * 0.15,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: height * 0.15,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
