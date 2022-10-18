6968dd3e8595e674f223f5c7fd5ec573a441412c
coutooo <manuelcouto10@ua.pt>
2022-10-18 18:28:20 +0100

    small bug

--- parkbuddy/lib/Screens/Welcome/register_page.dart
+++ parkbuddy/lib/Screens/Welcome/register_page.dart
@@ -12,8 +12,10 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   // text controllers
+  final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _confirmpasswordController = TextEditingController();
 
   Future signIn() async {
     //await FirebaseAuth.instance.signInWithEmailAndPassword(
@@ -28,144 +30,173 @@ class _RegisterPageState extends State<RegisterPage> {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //logo
-              Transform.scale(
-                scale: 0.75,
-                child: Image(
-                  image: AssetImage(
-                    'assets/images/logo-no-background.png',
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //logo
+                Transform.scale(
+                  scale: 0.75,
+                  child: Image(
+                    image: AssetImage(
+                      'assets/images/logo-no-background.png',
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 75,
-              ),
+                SizedBox(
+                  height: 75,
+                ),
 
-              // text
-              Text(
-                "Welcome",
-                //style: GoogleFonts.bebasNeue(
-                //  fontSize: 52,
-                //),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Park with us!",
-                style: TextStyle(
-                  fontSize: 18,
+                // text
+                Text(
+                  "Welcome",
+                  //style: GoogleFonts.bebasNeue(
+                  //  fontSize: 52,
+                  //),
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              //name  textfield
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.deepPurple),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    hintText: 'Name',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Park with us!",
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              //email textfield
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
+                SizedBox(
+                  height: 50,
+                ),
+                //name  textfield
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: 'Name',
+                      fillColor: Colors.grey[200],
+                      filled: true,
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.deepPurple),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    hintText: 'Email',
-                    fillColor: Colors.grey[200],
-                    filled: true,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
+                SizedBox(
+                  height: 10,
+                ),
+                //email textfield
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: 'Email',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
 
-              //password textfield
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
+                //password textfield
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: 'Password',
+                      fillColor: Colors.grey[200],
+                      filled: true,
                     ),
-                    hintText: 'Password',
-                    fillColor: Colors.grey[200],
-                    filled: true,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              // confirm password textfield
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
+                SizedBox(
+                  height: 10,
+                ),
+                // confirm password textfield
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: _confirmpasswordController,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: 'Confirm Password',
+                      fillColor: Colors.grey[200],
+                      filled: true,
                     ),
-                    hintText: 'Confirm Password',
-                    fillColor: Colors.grey[200],
-                    filled: true,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              // sign in button
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: ((context) => LoginPage())));
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(160, 5, 10, 40),
-                      borderRadius: BorderRadius.circular(12),
+                SizedBox(
+                  height: 10,
+                ),
+                // sign in button
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: ((context) => LoginPage())));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(160, 5, 10, 40),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Center(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: ((context) => LoginPage())));
+                          },
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                    child: Center(
-                      child: GestureDetector(
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Center(
+                    child: GestureDetector(
                         onTap: () {
                           Navigator.push(
                               context,
@@ -173,38 +204,14 @@ class _RegisterPageState extends State<RegisterPage> {
                                   builder: ((context) => LoginPage())));
                         },
                         child: Text(
-                          'Sign Up',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    ),
+                          'Already have an account? Sign In',
+                          style:
+                              TextStyle(decoration: TextDecoration.underline),
+                        )),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: Center(
-                  child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: ((context) => LoginPage())));
-                      },
-                      child: Text(
-                        'Already have an account? Sign In',
-                        style: TextStyle(decoration: TextDecoration.underline),
-                      )),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
